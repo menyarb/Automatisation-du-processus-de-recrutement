535997c9d2a6550897dc589c16fc9dd34ba807c6
lina <benmossalina@iit.ens.tn>
2024-03-23 00:57:26 +0100

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import ReactDOM from "react-dom/client";
+import ListeCandidats from "./ListeCandidats";
+import Home from "./Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from './navbar'; // Utilisez NavBar au lieu de navBar
 
 function App() {
- return(
-  <div className="App">
-    
-  </div>
- )
-}
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <Routes>
+        <Route path="/" element={<Home />} />
 
+          <Route path="/ListeCandidats" element={<ListeCandidats />}>
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,2 @@
+export default function BasicTable() {
+    return (<h1>Home Page</h1>)}
